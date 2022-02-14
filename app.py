fd11ee3f4bd010f205b3bc9ded9fcb377fb820ec
scottamass <scot.atkinson@live.co.uk>
2022-02-14 11:22:12 +0000

    displays comics

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template,url_for,request
 from main import *
 app = Flask(__name__)
 
@@ -6,6 +6,26 @@ app = Flask(__name__)
 def index():
     out= get_heros()
     return render_template('index.html', heros=out )
+
+@app.route('/search',methods=['POST'])
+def search():
+   if request.method =='POST': 
+    search=request.form.get('hero')
+    out= search_hero(search)
+    return render_template('index.html', heros=out, search=search )  
+
+
+@app.route('/hero/<id>')
+def display(id):
+    out= display_hero(id)
+    comics = get_comics(id)
+    for i in out:
+        print(i['name'])
+       
+    return render_template('results.html',out=out,comics=comics )
+
+    
+        
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 
 
 
-from pprint import pprint as pp
+
 timestamp= datetime.datetime.now().strftime('%Y-%m-%d%H:%M:%S')
 authPub="f2c645519a3fbb515476990b9992eb9d"
 authPriv="cce38df6ee68a724906f282645263593aaf9003f"
@@ -17,36 +17,54 @@ def hash_params():
     hash_md5 = hashlib.md5()
     hash_md5.update(f'{timestamp}{authPriv}{authPub}'.encode('utf-8'))
     hashed_params = hash_md5.hexdigest()
-
+    
     return hashed_params
+params = {'ts': timestamp, 'apikey': authPub, 'hash': hash_params()}
 
 def get_heros():
-    params = {'ts': timestamp, 'apikey': authPub, 'hash': hash_params()}
+    
     all = requests.get(f'{baseurl}/characters',params=params).json()
     heros= all["data"]['results']
     return heros
 
 
-def search_hero():    
-    char=input('who would you like to search ?')
-    params = {'ts': timestamp, 'apikey': authPub, 'hash': hash_params()}
-    response = requests.get(f'{baseurl}/characters?name={char}',params=params).json()
-
-    if char == "exit":
-        exit()
+def search_hero(search):    
     
-    try:
-        parsed_json = response
-        name = parsed_json["data"]["results"][0]["name"]
-        desc = parsed_json["data"]["results"][0]["description"]
-        comics = parsed_json["data"]["results"][0]["comics"]['items']
-        print(name)
-        print(desc)
-        for item in comics:
-            print(item['name'])
+    params = {'ts': timestamp, 'apikey': authPub, 'hash': hash_params()}
+    response = requests.get(f'{baseurl}/characters?nameStartsWith={search}',params=params).json()
+    hero=response["data"]['results']
+    return hero
+    #try:
+    #    parsed_json = response
+    #    name = parsed_json["data"]["results"][0]["name"]
+    #    desc = parsed_json["data"]["results"][0]["description"]
+    #    comics = parsed_json["data"]["results"][0]["comics"]['items']
+    #    print(name)
+    #    print(desc)
+    #    for item in comics:
+    #        print(item['name'])
 
-    except IndexError:
-        print('not found')
+    #except IndexError:
+    #    print('not found')
+def display_hero(characterId):
+    params = {'ts': timestamp, 'apikey': authPub, 'hash': hash_params()}
+    response= requests.get(f'{baseurl}/characters/{characterId}',params=params).json()
+    results = response['data']['results']
+    return results
 
-#while True:
-#    search_hero()
+
+def get_comics(characterId):
+    params = {'ts': timestamp, 'apikey': authPub, 'hash': hash_params(), 'limit':50}
+    response= requests.get(f'{baseurl}/characters/{characterId}/comics',params=params).json()
+    comics = response['data']['results']
+    return comics
+    
+def load_more(characterId):
+    ofset = 50
+    params = {'ts': timestamp, 'apikey': authPub, 'hash': hash_params(), 'ofset':ofset}
+    response= requests.get(f'{baseurl}/characters/{characterId}/comics',params=params).json()
+    comics = response['data']['results']
+    ofset += ofset
+    print(ofset)
+    return comics
+        
